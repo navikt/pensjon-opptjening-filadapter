08d4685f9bd2450f6bda72c27a03e184cd65306f
Jan Kroken <jan.kroken@nav.no>
2025-01-30 12:49:46 +0100

    oppdaterte avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val navTokenSupportVersion = "5.0.14"
+val navTokenSupportVersion = "5.0.16"
 val logbackEncoderVersion = "8.0"
 val postgresqlVersion = "42.7.5"
 // val flywayCoreVersion = "11.1.1"
@@ -11,7 +11,7 @@ val flywayCoreVersion = "11.2.0"
 val testcontainersVersion = "1.20.4"
 val jacksonVersion = "2.18.2"
 val azureAdClient = "0.0.7"
-val assertjVersion = "3.27.2"
+val assertjVersion = "3.27.3"
 val awaitilityVersion = "4.2.2"
 val wiremockVersion = "3.10.0"
 val micrometerRegistryPrometheusVersion = "1.14.3"
@@ -19,7 +19,7 @@ val mockitoKotlinVersion = "5.4.0"
 val unleashVersion = "9.2.6"
 val jsonUnitVersion = "4.1.0"
 val guavaVersion = "33.4.0-jre"
-val jschVersion = "0.2.22"
+val jschVersion = "0.2.23"
 // val httpClient5Version = "5.3.1" // TODO: 5.4 feiler med NoClassDefFoundError
 val httpClient5Version = "5.4.1"
 val hibernateValidatorVersion = "8.0.1.Final"
@@ -29,11 +29,11 @@ val snakeYamlVersion = "2.3"
 val apacheSshdVersion = "2.14.0"
 
 plugins {
-    val kotlinVersion = "2.1.0"
+    val kotlinVersion = "2.1.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
-    id("org.springframework.boot") version "3.4.1"
-    id("com.github.ben-manes.versions") version "0.51.0"
+    id("org.springframework.boot") version "3.4.2"
+    id("com.github.ben-manes.versions") version "0.52.0"
 }
 
 apply(plugin = "io.spring.dependency-management")
