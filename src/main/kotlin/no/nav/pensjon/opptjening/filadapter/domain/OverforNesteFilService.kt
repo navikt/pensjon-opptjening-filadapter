67aaea89ef2802ff30d69e1a225596431061027d
Jan Kroken <jan.kroken@nav.no>
2025-05-12 22:39:20 +0200

    skrudde på automatisk overføring av filer, men filterer bort tomme filer

--- src/main/kotlin/no/nav/pensjon/opptjening/filadapter/domain/OverforNesteFilService.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/filadapter/domain/OverforNesteFilService.kt
@@ -1,6 +1,7 @@
 package no.nav.pensjon.opptjening.filadapter.domain
 
 import no.nav.pensjon.opptjening.filadapter.remote.filsluse.FilsluseKlient
+import no.nav.pensjon.opptjening.filadapter.remote.filsluse.RemoteFilInfo
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -12,16 +13,18 @@ class OverforNesteFilService(
     companion object {
         private val log : Logger = LoggerFactory.getLogger(OverforNesteFilService::class.java)
     }
-    var utestående: MutableSet<String> = mutableSetOf()
+    var utestående: MutableSet<RemoteFilInfo> = mutableSetOf()
 
     private fun finnFilerHvisIngenUtestående() {
         log.info("sjekker hvor mange filer som er cachet for overføring: ${utestående.size}")
         if (utestående.isEmpty()) {
             utestående.addAll(
-                filsluseKlient.scanForFiles("/outbound").map { it.name }
+                filsluseKlient.scanForFiles("/outbound")
             )
             log.info("utestående etter å ha blitt fyllt på med nye filer: ${utestående.size}")
-            utestående.removeIf { lagerstatusService.erLagret(it) }
+            utestående.removeIf { it.size == 0L }
+            log.info("utestående etter å ha fjernet tomme filer: ${utestående.size}")
+            utestående.removeIf { lagerstatusService.erLagret(it.name) }
             log.info("utestående etter å ha fjernet allerede overførte filer: ${utestående.size}")
         }
     }
@@ -29,25 +32,26 @@ class OverforNesteFilService(
     @Synchronized
     fun overforEnUteståendeFil() {
         finnFilerHvisIngenUtestående()
-        utestående.firstOrNull()?.let {
-            log.info("overfører $it")
+        utestående.firstOrNull()?.let { filinfo ->
+            val filnavn = filinfo.name
+            log.info("overfører $filnavn")
             val resultat = prosesserFilService.overførFil(
                 dir = "outbound",
-                filnavn = it,
+                filnavn = filnavn,
                 blockSize = 100_000
             )
             when (resultat.status) {
                 ProsesserFilService.OverførResultat.Status.OK -> {
-                    log.info("fil overført $it")
-                    utestående.remove(it)
+                    log.info("fil overført $filnavn")
+                    utestående.remove(filinfo)
                 }
 
                 ProsesserFilService.OverførResultat.Status.FINNES_IKKE_I_FILSLUSE -> {
-                    log.warn("fil forsøkt overført, men fantes ikke i filsluse: $it")
-                    utestående.remove(it)
+                    log.warn("fil forsøkt overført, men fantes ikke i filsluse: $filnavn")
+                    utestående.remove(filinfo)
                 }
                 ProsesserFilService.OverførResultat.Status.FEILET -> {
-                    log.error("overføring av fil feilet: $it")
+                    log.error("overføring av fil feilet: $filnavn")
                 }
             }
         }
