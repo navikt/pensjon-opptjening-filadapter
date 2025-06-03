ab6ebb287f28c089315bf013b4a9fb40852ae366
Jan Kroken <jan.kroken@nav.no>
2025-06-03 10:18:29 +0200

    nytt loggoppsett - pågående

--- src/main/kotlin/no/nav/pensjon/opptjening/filadapter/Application.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/filadapter/Application.kt
@@ -1,6 +1,7 @@
 package no.nav.pensjon.opptjening.filadapter
 
 import no.nav.pensjon.opptjening.filadapter.log.NAVLog
+import org.slf4j.MarkerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -10,6 +11,6 @@ class Application
 fun main(args: Array<String>) {
     val log = NAVLog(Application::class)
     log.open.info("Starting application")
-    log.team.info("Starting application")
+    log.team.info(MarkerFactory.getMarker("TEAM_LOGS"), "Starting application")
     runApplication<Application>(*args)
 }
