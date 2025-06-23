b74bdf1537d5d280efb19ae138d727a455f12a9c
Arild Birkeland <arild.birkeland@nav.no>
2025-06-23 10:42:10 +0200

    bare sjekk nye utestående filer mot popp

--- src/main/kotlin/no/nav/pensjon/opptjening/filadapter/domain/OverforNesteFilService.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/filadapter/domain/OverforNesteFilService.kt
@@ -11,21 +11,29 @@ class OverforNesteFilService(
     val prosesserFilService: ProsesserFilService,
 ) {
     companion object {
-        private val log : Logger = LoggerFactory.getLogger(OverforNesteFilService::class.java)
+        private val log: Logger = LoggerFactory.getLogger(OverforNesteFilService::class.java)
     }
+
     var utestående: MutableSet<RemoteFilInfo> = mutableSetOf()
+    var alleredeProsessert: MutableSet<RemoteFilInfo> = mutableSetOf()
 
     private fun finnFilerHvisIngenUtestående() {
         log.info("sjekker hvor mange filer som er cachet for overføring: ${utestående.size}")
         if (utestående.isEmpty()) {
-            utestående.addAll(
-                filsluseKlient.scanForFiles("/outbound")
-            )
-            log.info("utestående etter å ha blitt fyllt på med nye filer: ${utestående.size}")
-            utestående.removeIf { it.size == 0L }
-            log.info("utestående etter å ha fjernet tomme filer: ${utestående.size}")
-            utestående.removeIf { lagerstatusService.erLagret(it.name) }
-            log.info("utestående etter å ha fjernet allerede overførte filer: ${utestående.size}")
+            val kandidater = filsluseKlient.scanForFiles("/outbound")
+                .asSequence()
+                .filterNot { it in alleredeProsessert }
+                .filter { it.size > 0L }
+                .toList()
+
+            log.info("Fant ${kandidater.size} kandidatfiler etter filtrering av prosesserte og tomme filer.")
+
+            val (alleredeLagret, nyeUtestående) = kandidater.partition { lagerstatusService.erLagret(it.name) }
+
+            alleredeProsessert.addAll(alleredeLagret)
+            utestående.addAll(nyeUtestående)
+
+            log.info("${alleredeLagret.size} filer var allerede overført. ${nyeUtestående.size} nye filer lagt til for overføring.")
         }
     }
 
@@ -50,6 +58,7 @@ class OverforNesteFilService(
                     log.warn("fil forsøkt overført, men fantes ikke i filsluse: $filnavn")
                     utestående.remove(filinfo)
                 }
+
                 ProsesserFilService.OverførResultat.Status.FEILET -> {
                     log.error("overføring av fil feilet: $filnavn")
                 }
